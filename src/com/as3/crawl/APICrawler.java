de644cc4667b5d45f40d1f61018d92972c513035
rahulr56 <rahulr301@gmail.com>
2017-10-14 09:25:51 -0400

    Adding he final parser to find clear function and also prints all the APIs in all webpages to a file

--- src/com/as3/crawl/APICrawler.java
+++ src/com/as3/crawl/APICrawler.java
@@ -1,36 +1,44 @@
 package com.as3.crawl;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class APICrawler {
 
 	public void createLinks(ArrayList<String> APILinks) throws NoSuchAlgorithmException {
-
-		String test = APILinks.get(6).toString();
-		System.out.println(test);
 		String fixedURL = "http://help.adobe.com/en_US/FlashPlatform/reference/actionscript/3/";
-		String generatedURL = fixedURL + test + ".html";
-		System.out.println(generatedURL);
-
+		BufferedWriter bw = null;
 		try {
-			Document doc = Jsoup
-					.connect(
-							"http://help.adobe.com/en_US/FlashPlatform/reference/actionscript/3/flash/utils/ByteArray.html")
-					.get();
-			
-			Elements links = doc.getElementsByAttributeValue("href", "#clear()");
-			System.out.println(links);
-			
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
+			 bw = new BufferedWriter(new FileWriter("/home/f4lc0n/Desktop/crawl.txt"));
+			 for (String link : APILinks) {
+					String generatedURL = fixedURL + link.toString() + ".html";
+					
+						Document doc = Jsoup.connect(generatedURL).get();
+						
+						Elements methods = doc.getElementsByClass("signatureLink");
 
+						Elements clearMethods = doc.getElementsByAttributeValue("href", "#clear()");
+						if(clearMethods.size()!=0)
+							System.out.println(link.toString() + "\n" + clearMethods);
+						bw.write(methods.toString());
+				}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
